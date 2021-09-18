f457eb364f8049f1fca0681a32b6b45d88ef2806
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-09-18 21:06:02 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,9 +1,5 @@
 package batailleNavale;
 
-import java.awt.HeadlessException;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public abstract class Joueur {
 	
 	public final static int TOUCHE = 1;
@@ -15,57 +11,48 @@ public abstract class Joueur {
 	private int tailleGrille;
 	private String nom;
 	
-	
-	
 	public Joueur(int tailleGrille, String nom) {
-		this.nom = nom;
 		this.tailleGrille = tailleGrille;
-		
-				
+		this.nom = nom;
 	}
 	
 	public Joueur(int tailleGrille) {
 		this.tailleGrille = tailleGrille;
-		 this.nom="joueur";
-	
 	}
 	
 	public int getTailleGrille() {
 		return this.tailleGrille;
 	}
-	
-	public String getNom() {
-		return this.nom;
-		}
-	
-	public void jouerAvec(Joueur j) {
-		    this.adversaire = j;
-			j.adversaire = this;
-	
-		try {
-			deroulementJeu (this,j);
-		} catch (HeadlessException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private static void deroulementJeu(Joueur attaquant, Joueur defenseur) throws HeadlessException, FileNotFoundException, IOException {
-		int res = 0;
-		while (res != GAMEOVER) {
-		Coordonnee c = attaquant.choisirAttaque();
-		res = defenseur.defendre(c);
-		attaquant.retourAttaque(c, res);
-		defenseur.retourDefense(c, res);
-		Joueur x = attaquant;
-		attaquant = defenseur;
-		defenseur = x;
-		}
-		}
 
+    public String getNom() {
+    return this.nom;
+    }
+    
+    public void jouerAvec(Joueur j) {
+    	j.adversaire = this;
+    	adversaire = j;
+    	deroulementJeu(this, j); 
+    }
+    
+    private static void deroulementJeu(Joueur attaquant, Joueur defenseur) {
+    	int res = 0;
+    	while (res != GAMEOVER) {
+    	   Coordonnee c = attaquant.choixAttaque();
+    	   res = defenseur.defendre(c);
+    	   attaquant.retourAttaque(c, res);
+    	   defenseur.retourDefense(c, res);
+    	   Joueur x = attaquant;
+    	   attaquant = defenseur; 
+    	   defenseur = x;
+    	}
+    	
+   }
+
+    protected abstract void retourAttaque(Coordonnee c, int etat);
+	protected abstract void retourDefense(Coordonnee c, int etat);
+	public abstract Coordonnee choixAttaque();
+	public abstract int defendre(Coordonnee c);
+    
+	
 
 }
