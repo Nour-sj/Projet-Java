642a32e41b6798619d73c5f8d3504a8e6fac8427
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-09-18 21:27:48 +0200

    Update JoueurAvecGrille.java

--- JoueurAvecGrille.java
+++ JoueurAvecGrille.java
@@ -1,37 +1,45 @@
 package batailleNavale;
 
-public abstract class JoueurAvecGrille extends Joueur {
-	private GrilleNavale grille;
+import java.util.Scanner;
+public class JoueurTexte extends JoueurAvecGrille {
+	private Scanner sc = new Scanner(System.in);
 	
-	
-	public JoueurAvecGrille(GrilleNavale g, String nom) {
-		super(g.getTaille(),nom);
-		this.grille=g;
-	}
-
-	public JoueurAvecGrille (GrilleNavale g) {
-		super(g.getTaille());
-		this.grille=g;
-	}
-	
-	public GrilleNavale getGrille() {
-		return grille;
-	}
-	
-	
-	public int defendre(Coordonnee c) {
-		grille.recoitTir(c);
-		if(grille.estCoule(c)) {
-			if (grille.perdu())
-				return GAMEOVER;
-			return COULE;
+	public JoueurTexte(GrilleNavale g, String nom ) {
+		super(g, nom);
 		}
-		if(grille.estALEau(c))
-			return A_L_EAU;
-		if (grille.estTouche(c))
-			return TOUCHE;
-		throw new RuntimeException ("La grille ne reçoit pas la coordonnée du tir");
+	
+	public JoueurTexte(GrilleNavale g) {
+		super(g);
 	}
-
-
+	
+	protected void retourAttaque(Coordonnee c, int etat) {
+		if(etat == 1) 
+			System.out.println("Vous avez touché un navire en "+ c +'\n');
+		 if(etat == 2) 
+			System.out.println("Vous avez coulé le navire en " + c +'\n');
+		 if(etat == 3) 
+			System.out.println("Vous avez tirez dans l'eau en " + c +'\n');
+		 if(etat == 4) 
+			System.out.println("WINNER : "+ this.getNom());
+	}
+	
+	protected void retourDefense(Coordonnee c, int etat) {
+		 if(etat == 1) 
+			System.out.println("Vous avez été touché en "+ c +'\n');
+		 if(etat == 2) 
+			System.out.println("Votre navire a coulé en " + c +'\n');
+		 if(etat == 3) 
+			System.out.println("Le tir précédent est allé à l'eau en " + c +'\n');
+		 if(etat == 4) 
+			System.out.println("GAMEOVER : " + this.getNom());
+	}
+	
+	public Coordonnee choixAttaque () {
+		System.out.println(this.getGrille());
+		System.out.println("Entrez la coordonnée d'attaque");
+		String input = sc.next();
+		Coordonnee c = new Coordonnee(input);
+		
+		return c;
+	}
 }
